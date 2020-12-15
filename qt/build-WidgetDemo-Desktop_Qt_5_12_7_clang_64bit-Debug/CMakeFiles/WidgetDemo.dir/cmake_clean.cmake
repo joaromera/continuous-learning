1431621d3e82cc04280925f2c779ece186228a8d
Joaquin Romera <joakromera@gmail.com>
2020-12-15 07:03:12 -0300

    qt message tutorial

---
+++ qt/build-WidgetDemo-Desktop_Qt_5_12_7_clang_64bit-Debug/CMakeFiles/WidgetDemo.dir/cmake_clean.cmake
@@ -0,0 +1,14 @@
+file(REMOVE_RECURSE
+  "WidgetDemo_autogen"
+  "CMakeFiles/WidgetDemo_autogen.dir/AutogenOldSettings.cmake"
+  "CMakeFiles/WidgetDemo.dir/main.cpp.o"
+  "CMakeFiles/WidgetDemo.dir/widget.cpp.o"
+  "CMakeFiles/WidgetDemo.dir/WidgetDemo_autogen/mocs_compilation.cpp.o"
+  "WidgetDemo.pdb"
+  "WidgetDemo"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/WidgetDemo.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ qt/build-WidgetDemo-Desktop_Qt_5_12_7_clang_64bit-Debug/CMakeFiles/WidgetDemo_autogen.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "WidgetDemo_autogen"
+  "CMakeFiles/WidgetDemo_autogen.dir/AutogenOldSettings.cmake"
+  "CMakeFiles/WidgetDemo_autogen"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang )
+  include(CMakeFiles/WidgetDemo_autogen.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
