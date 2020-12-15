0cf0af9d475b999b13e82a47fb08bc3a6f39b89b
Joaquin Romera <joakromera@gmail.com>
2020-12-15 07:02:48 -0300

    input for day7

---
+++ aoc2020/day07/main.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+#include <string>
+#include <sstream>
+#include <vector>
+
+std::vector<std::string> parse_input()
+{
+    std::stringstream ss;
+    for (std::string line; getline(std::cin, line); )
+        if (line.empty())
+            ss << '\n';
+        else
+            ss << ' ' << line;
+
+    std::vector<std::string> lines;
+    for (std::string line; getline(ss, line); )
+        lines.push_back(std::move(line));
+
+    return lines;
+}
+
+int main()
+{
+    const std::vector<std::string> input = parse_input();
+
+}
