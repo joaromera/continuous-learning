7a676507c33bb4b91681ff87a05acc780d26f16c
joaromera <joakromera@gmail.com>
2020-12-24 15:15:55 -0300

    added assertion and refactor

--- cpp/camelCaseSplit.cpp
+++ cpp/camelCaseSplit.cpp
@@ -1,26 +1,43 @@
 #include <iostream>
 #include <string>
 
-std::string functionName(const std::string& sv)
+std::string camelCaseSplit(std::string str)
 {
-    const std::string namePrefix = "test";
-    const std::string::size_type nameStartPos = sv.find(namePrefix) + namePrefix.length();
-    const std::string::size_type nameLength = sv.find("(") - nameStartPos;
-    std::string name = sv.substr(nameStartPos, nameLength);
-
-    for (auto it = name.begin(); it != name.end(); ++it)
+    for (auto it = str.begin(); it != str.end(); ++it)
     {
         if (std::isupper(*it))
         {
-            it = name.insert(it, ' ');
+            it = str.insert(it, ' ');
             ++it;
         }
     }
-    return name;
+    return str;
+}
+
+std::string parseTestName(const std::string& str)
+{
+    const std::string namePrefix = "test";
+
+    const std::string::size_type namePrefixPos = str.find(namePrefix);
+    const std::string::size_type nameEndPos = str.find('(');
+
+    if (namePrefixPos == std::string::npos ||
+        nameEndPos == std::string::npos ||
+        namePrefixPos > nameEndPos)
+    {
+        throw std::invalid_argument(
+            "Function names must start with 'test' and include a parameter list in ()"
+        );
+    }
+
+    const std::string::size_type nameStartPos = namePrefixPos + namePrefix.length();
+    const std::string::size_type nameLength = nameEndPos - nameStartPos;
+    const std::string name = str.substr(nameStartPos, nameLength);
+    return camelCaseSplit(name);
 }
 
 int main()
 {
-    std::cout << functionName("public void testCowsCanBeMilked()") << std::endl;
-    std::cout << functionName("public void testSheepAreNotTheOnlyFruit()") << std::endl;
+    std::cout << parseTestName("public void testCowsCanBeMilked()") << std::endl;
+    std::cout << parseTestName("public void testSheepAreNotTheOnlyFruit()") << std::endl;
 }
